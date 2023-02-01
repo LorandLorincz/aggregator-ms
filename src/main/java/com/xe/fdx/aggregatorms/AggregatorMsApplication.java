f39555d67e5b69194fe3c626d8a1e9cfc326222b
Lorand Lorincz <lorand.lorincz.87@gmail.com>
2023-02-01 22:10:11 +0200

    initial commit

---
+++ src/main/java/com/xe/fdx/aggregatorms/AggregatorMsApplication.java
@@ -0,0 +1,13 @@
+package com.xe.fdx.aggregatorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xe/fdx/aggregatorms/AggregatorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xe.fdx.aggregatorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
