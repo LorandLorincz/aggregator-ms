d1c84cf55ea650b439f1926bb0f9c266df7f9349
Lorand Lorincz <lorand.lorincz.87@gmail.com>
2023-02-04 19:49:01 +0200

    happy flow get pricing

--- src/main/java/com/xe/fdx/aggregator/business/AggregationService.java
+++ src/main/java/com/xe/fdx/aggregator/business/AggregationService.java
@@ -1,10 +1,11 @@
 package com.xe.fdx.aggregator.business;
 
+import com.xe.fdx.aggregator.external.PricingApi;
 import com.xe.fdx.aggregator.external.ShipmentsApi;
 import com.xe.fdx.aggregator.external.TrackApi;
 import com.xe.fdx.aggregator.model.TrackStatus;
 import com.xe.fdx.aggregator.model.Aggregation;
-import java.util.Collections;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -21,27 +22,38 @@ public class AggregationService {
   public final ShipmentsApi shipmentsApi;
   public final TrackApi trackApi;
 
+  public final PricingApi pricingApi;
+
   public Mono<Aggregation> getAggregation(List<String> shipmentsOrderNumbers,
       List<String> trackOrderNumbers, List<String> pricingCountryCodes) {
-    return Mono.zip(getShipmentsList(shipmentsOrderNumbers), getTrackStatus(trackOrderNumbers)).map(
-        t2 -> new Aggregation(t2.getT1().stream()
-            .collect(Collectors.toMap(Tuple2::getT1, Tuple2::getT2)),
-            t2.getT2().stream()
+    return Mono.zip(getShipmentsFlux(shipmentsOrderNumbers), getTrackStatusFlux(trackOrderNumbers),
+            getPricingFlux(pricingCountryCodes))
+        .map(
+            t3 -> new Aggregation(t3.getT1().stream()
                 .collect(Collectors.toMap(Tuple2::getT1, Tuple2::getT2)),
-            Collections.emptyMap()));
+                t3.getT2().stream()
+                    .collect(Collectors.toMap(Tuple2::getT1, Tuple2::getT2)),
+                t3.getT3().stream().collect(Collectors.toMap(Tuple2::getT1, Tuple2::getT2))));
   }
 
-  private Mono<List<Tuple2<String, List<String>>>> getShipmentsList(
+  private Mono<List<Tuple2<String, List<String>>>> getShipmentsFlux(
       List<String> shipmentsOrderNumbers) {
     return Flux.fromIterable(shipmentsOrderNumbers)
         .flatMap(o -> shipmentsApi.getShipments(o).map(l -> Tuples.of(o, l)))
         .collectList();
   }
 
-  private Mono<List<Tuple2<String, TrackStatus>>> getTrackStatus(
+  private Mono<List<Tuple2<String, TrackStatus>>> getTrackStatusFlux(
       List<String> trackOrderNumbers) {
     return Flux.fromIterable(trackOrderNumbers)
         .flatMap(o -> trackApi.getTrackingStatus(o).map(l -> Tuples.of(o, l)))
+        .collectList();
+  }
+
+  private Mono<List<Tuple2<String, BigDecimal>>> getPricingFlux(
+      List<String> countryCodes) {
+    return Flux.fromIterable(countryCodes)
+        .flatMap(c -> pricingApi.getPricing(c).map(l -> Tuples.of(c, l)))
         .collectList();
   }
 }
---
+++ src/main/java/com/xe/fdx/aggregator/external/PricingApi.java
@@ -0,0 +1,27 @@
+package com.xe.fdx.aggregator.external;
+
+import com.xe.fdx.aggregator.model.TrackStatus;
+import java.math.BigDecimal;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PricingApi {
+
+  private final WebClient client;
+
+  public PricingApi(WebClient.Builder builder) {
+    this.client = builder.baseUrl("http://localhost:4000").build();
+  }
+
+  public Mono<BigDecimal> getPricing(String countryCode) {
+    return this.client.get()
+        .uri(b -> b.path("/pricing").queryParam("countryCode", countryCode).build())
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono(BigDecimal.class)
+        .doOnNext(s -> System.out.println(s));
+  }
+}
