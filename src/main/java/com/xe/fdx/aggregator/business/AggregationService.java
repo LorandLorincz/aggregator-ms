487cd326775c5b157efb84d8993154d78ae2f125
Lorand Lorincz <lorand.lorincz.87@gmail.com>
2023-02-06 22:13:06 +0200

    packaging, readme, properties

--- src/main/java/com/xe/fdx/aggregator/business/AggregationService.java
+++ src/main/java/com/xe/fdx/aggregator/business/AggregationService.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -27,6 +28,13 @@ public class AggregationService {
 
   public final PricingApi pricingApi;
 
+  @Value("${tracking.api.timeout}")
+  private int trackingApiTimout;
+  @Value("${shipment.api.timeout}")
+  private int shippingApiTimout;
+  @Value("${pricing.api.timeout}")
+  private int pricingApiTimout;
+
   public Mono<Aggregation> getAggregation(List<String> shipmentsOrderNumbers,
       List<String> trackOrderNumbers, List<String> pricingCountryCodes) {
     return Mono.zip(getShipmentsFlux(shipmentsOrderNumbers), getTrackStatusFlux(trackOrderNumbers),
@@ -41,30 +49,43 @@ public class AggregationService {
 
   private Mono<List<Tuple2<String, List<String>>>> getShipmentsFlux(
       List<String> shipmentsOrderNumbers) {
-    return Flux.fromIterable(shipmentsOrderNumbers)
-        .flatMap(o -> shipmentsApi.getShipments(o).timeout(Duration.ofSeconds(5)).doOnError(
-                e -> log.info("Failed to get shipment status for {}. Error was: {}", o, e.getMessage()))
-            .onErrorResume(e -> Mono.empty())
-            .map(l -> Tuples.of(o, l)))
-        .collectList();
+    return Flux.fromIterable(shipmentsOrderNumbers).flatMap(this::getShipment).collectList();
+  }
+
+  private Mono<Tuple2<String, List<String>>> getShipment(String shipmentOrderNumber) {
+    return shipmentsApi.getShipments(shipmentOrderNumber)
+        .timeout(Duration.ofSeconds(shippingApiTimout))
+        .doOnError(
+            e -> log.info("Failed to get shipment status for {}. Error was: {}",
+                shipmentOrderNumber,
+                e.getMessage()))
+        .onErrorResume(e -> Mono.empty())
+        .map(l -> Tuples.of(shipmentOrderNumber, l));
   }
 
   private Mono<List<Tuple2<String, TrackStatus>>> getTrackStatusFlux(
       List<String> trackOrderNumbers) {
-    return Flux.fromIterable(trackOrderNumbers)
-        .flatMap(o -> trackApi.getTrackingStatus(o).timeout(Duration.ofSeconds(5)).doOnError(
-                e -> log.info("Failed to get tracking status for {}. Error was: {}", o, e.getMessage()))
-            .onErrorResume(e -> Mono.empty())
-            .map(l -> Tuples.of(o, l)))
-        .collectList();
+    return Flux.fromIterable(trackOrderNumbers).flatMap(this::getTrackingStatus).collectList();
+  }
+
+  private Mono<Tuple2<String, TrackStatus>> getTrackingStatus(String trackOrderNumber) {
+    return trackApi.getTrackingStatus(trackOrderNumber)
+        .timeout(Duration.ofSeconds(trackingApiTimout))
+        .doOnError(
+            e -> log.info("Failed to get tracking status for {}. Error was: {}", trackOrderNumber,
+                e.getMessage()))
+        .onErrorResume(e -> Mono.empty())
+        .map(l -> Tuples.of(trackOrderNumber, l));
   }
 
   private Mono<List<Tuple2<String, BigDecimal>>> getPricingFlux(
       List<String> countryCodes) {
-    return Flux.fromIterable(countryCodes)
-        .flatMap(c -> pricingApi.getPricing(c).timeout(Duration.ofSeconds(5)).doOnError(
-                e -> log.info("Failed to get pricing for {}. Error was: {}", c, e.getMessage()))
-            .onErrorResume(e -> Mono.empty()).map(l -> Tuples.of(c, l)))
-        .collectList();
+    return Flux.fromIterable(countryCodes).flatMap(this::getPricing).collectList();
+  }
+
+  private Mono<Tuple2<String, BigDecimal>> getPricing(String country) {
+    return pricingApi.getPricing(country).timeout(Duration.ofSeconds(pricingApiTimout)).doOnError(
+            e -> log.info("Failed to get pricing for {}. Error was: {}", country, e.getMessage()))
+        .onErrorResume(e -> Mono.empty()).map(l -> Tuples.of(country, l));
   }
 }
